a3dd38799155785c8b99926d84185f9e569b3b16
Winston <bjdhjj@gmail.com>
2018-04-19 22:39:24 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
 sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/default.conf
+sed -i "s/WALLET/$WALLET/g" /xmr-stak/build/bin/pools.txt
+
 mkdir -p /var/tmp/nginx/client_body
 # start nginx
 nginx
