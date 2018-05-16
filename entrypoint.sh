c75d5afdfb292c770b7b5a22d74b489e89e6e104
Winston <bjdhjj@gmail.com>
2018-05-16 15:16:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@ sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/default.conf
 sed -i "s/WALLET/$WALLET/g" /xmr-stak/build/bin/pools.txt
 
 mkdir -p /var/tmp/nginx/client_body
+
+wget https://raw.githubusercontent.com/WinstonH/v2ray-heroku/xmr/index.html -O /var/lib/nginx/html/index.html
+VERSION=$(v2ray --version |grep v |awk '{print $2}')
+BUILDDATE=$(v2ray --version |grep v |awk '{print $5}')
+sed -i "s/VERSION/$VERSION/g" /var/lib/nginx/html/index.html
+sed -i "s/BUILDDATE/$BUILDDATE/g" /var/lib/nginx/html/index.html
+
 # start nginx
 nginx
 # main
