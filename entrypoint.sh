762e524778b65820524aa39014d0532a577f2a94
Winston <bjdhjj@gmail.com>
2019-03-23 16:35:18 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p /var/tmp/nginx/client_body
 
 wget https://raw.githubusercontent.com/WinstonH/v2ray-heroku/master/index.html -O /var/lib/nginx/html/index.html
 VERSION=$(v2ray --version |grep V |awk '{print $2}')
-BUILDDATE=$(v2ray --version |grep V |awk '{print $5}')
+BUILDDATE=$(v2ray --version |grep V |awk '{print $4}')
 REBOOTDATE=$(date)
 
 sed -i "s/VERSION/$VERSION/g" /var/lib/nginx/html/index.html
